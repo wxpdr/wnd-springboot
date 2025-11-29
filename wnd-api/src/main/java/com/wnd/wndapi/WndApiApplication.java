56bbc7ee29dd9beb38935c85bd32af2c2210dd29
Wendy <euwendypedrosa@gmail.com>
2025-11-29 11:17:39 -0300

    Subindo Spring Cru

---
+++ wnd-api/src/main/java/com/wnd/wndapi/WndApiApplication.java
@@ -0,0 +1,13 @@
+package com.wnd.wndapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WndApiApplication.class, args);
+	}
+
+}
---
+++ wnd-api/src/test/java/com/wnd/wndapi/WndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnd.wndapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
